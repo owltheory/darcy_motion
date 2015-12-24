4880c87668ede4eee91145645ca836f086de8d01
Michael Shullick <mxs9342@rit.edu>
2015-12-24 19:26:07 +0000

    Testing teleop control. Potential bug in darcy_motion that mkes it unresponsive after a few seconds.

--- src/darcy_motion.cpp
+++ src/darcy_motion.cpp
@@ -1,23 +1,78 @@
-#include "ros/ros.h"
-#include "std_msgs/Float32.h"
+#include <ros/ros.h>
+#include <std_msgs/Float32.h>
 #include <wiringPi.h>
-#include "darcy_motion/Motion.h"
+#include <darcy_motion/Motion.h>
+#include <unistd.h> // for usleep
+#include <stdlib.h> // for system
 
+class DarcyMotion
+{
+    public:
+        DarcyMotion();
+        void spin();
+     
+    private:
+        void teleopCallback(const darcy_motion::Motion::ConstPtr& msg);
+        
+        int dir_, delayT_, steps_;
+        int stepPin = 4, dirPin = 28;
 
-void teleopCallback(const darcy_motion::Motion::ConstPtr& msg) {
+        ros::NodeHandle n;
+        ros::Subscriber teleop_sub_;
+};
+
+DarcyMotion::DarcyMotion():
+    dir_(0), delayT_(0), steps_(0)
+{
+    wiringPiSetupSys();
+    //pinMode(stepPin,OUTPUT);
+    //pinMode(dirPin,OUTPUT);
+    system("gpio export 4 out");
+    system("gpio export 28 out");
+
+    teleop_sub_ = n.subscribe<darcy_motion::Motion>("teleop_commands", 1000, 
+            &DarcyMotion::teleopCallback, this);
+}
+
+void DarcyMotion::teleopCallback(const darcy_motion::Motion::ConstPtr& msg) {
     ROS_INFO("Got %f", msg->wrist_piv);
+    delayT_ = msg->wrist_piv;
+    dir_ = (msg->wrist_piv > 0) ? 0 : 1;
+    steps_++;
+}
+
+void DarcyMotion::spin() {
+
+    if (steps_ > 0) {
+
+        digitalWrite(dirPin, dir_);
+
+        digitalWrite(stepPin, HIGH);
+        usleep(delayT_);
+        digitalWrite(stepPin,LOW);
+        usleep(delayT_);
+
+        steps_--;
+    }
 }
 
 
 int main (int argc, char **argv)
 {
+    // init
+    ROS_INFO("Initializing node darcy_motion...");
     ros::init(argc, argv, "darcy_motion");
+    DarcyMotion darcy_motion;
+    ROS_INFO("darcy_motion initialized. Waiting for input ...");
+    //ros::Rate loop_rate(100);
 
-    ros::NodeHandle n;
+    while (ros::ok()) {
 
-    ros::Subscriber sub = n.subscribe("teleop_commands", 1000, teleopCallback);
+        darcy_motion.spin();
+        ros::spinOnce();     
+     //   loop_rate.sleep();
 
-    ros::spin();
+    }
 
     return 0;
 }
